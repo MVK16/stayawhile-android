1e3c68e65a6c7326f29c7cab14ab59c476f52a4e
svedi <mvaiopwhgopilkj@gmail.com>
2016-06-03 10:31:59 +0200

    Back buttons start MainActivity

--- mobile/src/main/java/se/kth/csc/stayawhile/QueueActivity.java
+++ mobile/src/main/java/se/kth/csc/stayawhile/QueueActivity.java
@@ -471,9 +471,21 @@ public class QueueActivity extends AppCompatActivity implements MessageDialogFra
     }
 
     @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        this.finish();
+        startActivity(intent);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         final int id = item.getItemId();
         switch (id) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                this.finish();
+                startActivity(intent);
+                return true;
             case R.id.action_broadcast:
             case R.id.action_broadcast_faculty:
                 MessageDialogFragment fragment = new MessageDialogFragment();
